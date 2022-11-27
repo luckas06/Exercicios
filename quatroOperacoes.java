c26da02de02ede638d8f28dc2fb3cb3cbfce48ae
luul._.sepol <112173391+luckas06@users.noreply.github.com>
2022-11-27 10:12:02 -0300

    Update quatroOperacoes.java

--- quatroOperacoes.java
+++ quatroOperacoes.java
@@ -1,21 +1,21 @@
-package dozeEx;
+package Exercicios;
 import java.util.*;
-public class quatroOperacoes {
-	public static void main (String arg[]) {
-		Scanner in=new Scanner (System.in);
+public class QuatroOperacoes {
+	public static void main (String[]args) {
+		Scanner in = new Scanner (System.in);
 		double n1, n2, soma, sub, mult, div;
 		System.out.println("Digite um valor:");
-		n1=in.nextDouble();
+		n1 = in.nextDouble();
 		System.out.println("Digite outro valor:");
-		n2=in.nextDouble();
-		soma=n1+n2;
-		sub=n1-n2;
-		mult=n1*n2;
-		div=n1/n2;
-		System.out.println("A soma entre esse valores é:"+soma);
-		System.out.println("A subtração entre esses valores é:"+sub);
-		System.out.println("A mutiplicação entre esses dois valores é:"+mult);
-		System.out.println("A divisão entre esses dois valores é:"+div);
+		n2 = in.nextDouble();
+		soma = n1 + n2;
+		sub = n1 - n2;
+		mult = n1 * n2;
+		div = n1 / n2;
+		System.out.println("A soma entre esse valores é "+soma);
+		System.out.println("A subtração entre esses valores é "+sub);
+		System.out.println("A mutiplicação entre esses dois valores é: +mult);
+		System.out.println("A divisão entre esses dois valores é "+div);
 		in.close();
 	}
 }
