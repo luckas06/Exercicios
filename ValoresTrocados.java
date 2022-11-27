c93780e69090fdcacc03fcb611066f46548d6abe
luul._.sepol <112173391+luckas06@users.noreply.github.com>
2022-11-27 10:37:22 -0300

    Update ValoresTrocados.java

--- ValoresTrocados.java
+++ ValoresTrocados.java
@@ -1,19 +1,17 @@
-package dozeEx;
+package Exercicios;
 import java.util.*;
 public class ValoresTrocados {
-	public static void main (String arg[]){
-		Scanner in=new Scanner (System.in);
+	public static void main (String[])args{
+		Scanner in = new Scanner (System.in);
 		int a, b, c;
-		System.out.println("Digite o valor A:");
-		a=in.nextInt();
-		System.out.println("Digite o valor B:");
-		b=in.nextInt();
-		c=a;
-		a=b;
-		b=c;
-		System.out.println("O valor de A é:"+a);
-		System.out.println("O valor de B é:"+b);
-		in.close();
-	}
-
+		System.out.println("Digite o valor de A");
+		a = in.nextInt();
+		System.out.println("Digite o valor de B");
+		b = in.nextInt();
+		c = a;
+		a = b;
+		b = c;
+		System.out.println("O valor de A é "+a);
+		System.out.println("O valor de B é "+b);
+	}in.close();
 }
