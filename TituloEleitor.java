ce3bcc617b29abf1a21bca7aba5502ce142de8a5
luul._.sepol <112173391+luckas06@users.noreply.github.com>
2022-11-27 10:04:29 -0300

    Update and rename tituloEleitor.java to TituloEleitor.java

--- TituloEleitor.java
+++ TituloEleitor.java
@@ -1,15 +1,15 @@
-package dezDeJunho;
+package Exercicios;
 import java.util.*;
-public class tituloEleitor {
-	public static void main (String arg[]) {
-		Scanner in=new Scanner (System.in);
+public class TituloEleitor {
+	public static void main (String[]args) {
+		Scanner in = new Scanner (System.in);
 		int nasc, atual, idade;
 		System.out.println("Escreva seu ano de nascimento:");
-		nasc=in.nextInt();
+		nasc = in.nextInt();
 		System.out.println("Escreva o ano atual:");
-		atual=in.nextInt();
-		idade=atual-nasc;
-		if(idade<16) {
+		atual = in.nextInt();
+		idade = atual-nasc;
+		if (idade<16) {
 			System.out.println("Não pode tirar o título de eleitor");
 		}else {
 			System.out.println("Pode tirar o título de elitor");
