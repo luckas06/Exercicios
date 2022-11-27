d85b097e76e83577dd4c9b9233b221152c7e9365
luul._.sepol <112173391+luckas06@users.noreply.github.com>
2022-11-27 09:56:44 -0300

    Rename NumPrimo.java to NumPrimo2.java

--- NumPrimo2.java
+++ NumPrimo2.java
@@ -1,22 +1,23 @@
 package Exercicios;
 import java.util.*;
-public class NumPrimo2 {
+public class NumPrimo {
 	public static void main(String[]args) {
 		Scanner in = new Scanner(System.in);
 		double i, n, a=0;
 	
-		System.out.println("Digite um número");
+System.out.println("Digite um número");
 		n = in.nextDouble();
 	
 		for (i=1; i<=n; i++) {
 			if (n == 1) {
-				a = a + 2;
 			}
 			else if (n%i==0) {
 				a = a + 1;
 			}
+		}if (n == 1) {
+			System.out.println("1 não é composto e nem primo");
 		}
-		if (a == 2) {
+		else if (a == 2) {
 			System.out.println("O número é primo");
 		}else {
 			System.out.println("O número não é primo");
